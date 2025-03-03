9a506ba4fb20b7f83986c197f054dd1144cfd598
sachinthakur1912 <thakursachin1912@gmail.com>
2025-03-03 11:42:30 +0530

    chore:created coffes controller with get and post requests

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll(@Res() response) {
+    response.status(210).send('This action returns all coffees');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+}
